2525fc8aa902892d6674bd198f368fd793c9f610
denghuihui <908330896@qq.com>
2019-06-27 09:45:11 +0800

    multi proccess -> aidl

--- app/src/main/java/com/dhh/audition/multprocess_aidl/AidlMultiProcessActivity.kt
+++ app/src/main/java/com/dhh/audition/multprocess_aidl/AidlMultiProcessActivity.kt
@@ -1,10 +1,37 @@
 package com.dhh.audition.multprocess_aidl
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
+import com.dhh.audition.IMyAidlInterface
+import com.dhh.audition.R
+import kotlinx.android.synthetic.main.activity_aidl_multi_process.*
 
 class AidlMultiProcessActivity: AppCompatActivity() {
+    var iMyAidlInterface: IMyAidlInterface? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aidl_multi_process)
+        button3.setOnClickListener {
+                Thread(Runnable {
+                    val result = iMyAidlInterface?.basicTypes(111)
+                    println("客户端收到消息了：$result" )
+                }).start()
+        }
+        bindService(Intent(this, AidlService::class.java), serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            iMyAidlInterface = IMyAidlInterface.Stub.asInterface(service)
+        }
+
     }
 }
--- app/src/main/java/com/dhh/audition/multprocess_aidl/AidlService.kt
+++ app/src/main/java/com/dhh/audition/multprocess_aidl/AidlService.kt
@@ -1,17 +1,27 @@
 package com.dhh.audition.multprocess_aidl
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import com.dhh.audition.IMyAidlInterface
 
 class AidlService: Service(){
+    var mContext: Context? = null
+    override fun onCreate() {
+        super.onCreate()
+        mContext = baseContext
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         return AidlBinder()
     }
     class AidlBinder: IMyAidlInterface.Stub() {
-        override fun basicTypes(a: Int) {
-
+        override fun basicTypes(aInt: Int): String {
+            println("服务端收到消息了")
+            Thread.sleep(5000)
+            return "ssssss"
         }
+
     }
 }
